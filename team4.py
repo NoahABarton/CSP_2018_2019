4ab84820e452c7c60a6fc0f1c995da263d1341bb
NoahABarton <noah.barton@mastersny.org>
2018-12-21 10:48:13 -0500

    Update team4.py

--- team4.py
+++ team4.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'BillShank' # Only 10 chars displayed.
+team_name = 'BigBoiName' # Only 10 chars displayed.
 strategy_name = 'MAGA'
 strategy_description = 'Irrational Moves: If winning change.'
 def move(my_history, their_history, my_score, their_score):
